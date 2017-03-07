6a0a19802213965270908b5df55b146d9e73d891
kasireddy577 <tkrdevops@gmail.com>
2017-03-07 11:53:24 +0530

    Update src.java

--- src.java
+++ src.java
@@ -1,11 +1,8 @@
 public class HelloWorldExample{
  
   public static void main(String args[]){
- 
-    /*
-    Use System.out.println() to print on console.
-    */
-    System.out.println("Hello World !");
+     Use System.out.println() to print on console.
+     System.out.println("Hello World !");
  
   }
  
